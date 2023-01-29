bc4b5cf98fd983f11487407468c04566fb03037e
kiko <2224514@slu.edu.ph>
2023-01-29 22:18:01 +0800

    - Added URL Reader

---
+++ src/JavaURL/URLReader.java
@@ -0,0 +1,18 @@
+package JavaURL;
+
+import java.net.*;
+import java.io.*;
+
+public class URLReader {
+    public static void main(String[] args) throws Exception {
+
+        URL oracle = new URL("https://www.youtube.com/results?search_query=knock+knock+server");
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+
+    }
+}
